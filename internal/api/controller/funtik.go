6d191634b1ddaba12eacb158661be122d23460c2
ilianovikov <ilianovikov@ozon.ru>
2022-11-20 18:41:06 +0300

    funtik

--- internal/api/controller/funtik.go
+++ internal/api/controller/funtik.go
@@ -35,7 +35,7 @@ func (c *Controller) SubscribeToFuntik(ctx echo.Context) error {
 		Number:   uuid.New(),
 		UserID:   request.UserID,
 		Currency: "RUB",
-		Balance:  decimal.NewFromInt(0),
+		Balance:  decimal.NewFromInt(300000),
 		ForBot:   true,
 	}
 	err = c.store.CreateAccount(ctx.Request().Context(), &funtikRUBAcc)
@@ -55,7 +55,7 @@ func (c *Controller) SubscribeToFuntik(ctx echo.Context) error {
 	}
 	user, err := c.store.GetUserByID(ctx.Request().Context(), request.UserID)
 	// запускаем фонового бота
-	go runFuntik(ctx.Request().Context(), user, &funtikRUBAcc, &funtikUSDAcc)
+	go c.runFuntik(ctx.Request().Context(), user, &funtikRUBAcc, &funtikUSDAcc)
 	// да немного поломал архитектуру контроллер-сервис-репо, но если честно я немного не успеваю, да и Оганес
 	// сам ее уже сломал до меня!!
 	logger.Error(ctx.Request().Context(), acc)
@@ -83,32 +83,80 @@ type funtikStuff struct {
 	DipThreshold         float64
 	ProfitThreshold      float64
 	StopLossThreshold    float64
-	LastOpPrice          float64
-	CurrentPrice         float64
+	LastOpPrice          decimal.Decimal
+	CurrentPrice         decimal.Decimal
+
+	accountRUB *core.Account
+	accountUSD *core.Account
 }
 
-func runFuntik(ctx context.Context, user *core.User, funtikRUBAcc, funtikUSDAcc *core.Account) {
+func (c *Controller) runFuntik(ctx context.Context, user *core.User, funtikRUBAcc, funtikUSDAcc *core.Account) {
 	stuff := funtikStuff{
 		IsNextOperationBuy:   true,
 		UpwardTrendThreshold: 1.50,
 		DipThreshold:         -2.25,
 		ProfitThreshold:      1.25,
 		StopLossThreshold:    -2.00,
+
+		accountRUB: funtikRUBAcc,
+		accountUSD: funtikUSDAcc,
 	}
-	stuff.decideToBuy()
 	for {
 		if user.SubscriptionExpiredAt != nil && user.SubscriptionExpiredAt.After(time.Now()) {
 			break
 		}
-		logger.Errorf(ctx, "funtik does something")
-		// do some work
+		err := c.decideToBuy(ctx, &stuff)
+		if err != nil {
+			logger.Errorf(ctx, "error in funtik occures: %w", err)
+			break
+		}
 		time.Sleep(1 * time.Second)
 	}
 }
 
-func (c *funtikStuff) decideToBuy() bool {
-	//f
-	//
-	//currentPrice :=
-	return false
+func (c *Controller) decideToBuy(ctx context.Context, staff *funtikStuff) error {
+	var err error
+	var resp *dto.MakePurchaseResponse
+	var percentageDiff float64
+
+	percentageDiff = (staff.CurrentPrice.Sub(staff.LastOpPrice)).Div(staff.LastOpPrice).InexactFloat64() * float64(100)
+
+	if staff.IsNextOperationBuy {
+		// try to buy
+		if percentageDiff <= staff.UpwardTrendThreshold || percentageDiff <= staff.DipThreshold {
+			staff.CurrentPrice, err = c.service.LatestCurrencyPrice(ctx, staff.accountUSD.Currency, staff.accountRUB.Currency)
+			resp, err = c.service.MakeTransfer(ctx, &dto.TransferRequestDTO{
+				AccountFrom:       staff.accountRUB.Number,
+				CreditAmountCents: staff.accountRUB.Balance.Mul(decimal.NewFromFloat(0.1)),
+				AccountTo:         staff.accountUSD.Number,
+				DebitAmountCents:  staff.accountRUB.Balance.Mul(decimal.NewFromFloat(0.1)).Mul(staff.CurrentPrice),
+			}, staff.CurrentPrice)
+			if err != nil {
+				return err
+			}
+			staff.accountRUB = resp.NewAccountFrom
+			staff.accountUSD = resp.NewAccountTo
+			staff.LastOpPrice = staff.CurrentPrice
+			staff.IsNextOperationBuy = false
+		}
+	} else {
+		// try to sell
+		staff.CurrentPrice, err = c.service.LatestCurrencyPrice(ctx, staff.accountRUB.Currency, staff.accountUSD.Currency)
+		if percentageDiff >= staff.ProfitThreshold || percentageDiff <= staff.StopLossThreshold {
+			resp, err = c.service.MakeTransfer(ctx, &dto.TransferRequestDTO{
+				AccountFrom:       staff.accountUSD.Number,
+				CreditAmountCents: staff.accountUSD.Balance.Mul(decimal.NewFromFloat(0.1)),
+				AccountTo:         staff.accountRUB.Number,
+				DebitAmountCents:  staff.accountRUB.Balance.Mul(decimal.NewFromFloat(0.1)).Mul(staff.CurrentPrice),
+			}, staff.CurrentPrice)
+			if err != nil {
+				return err
+			}
+			staff.accountUSD = resp.NewAccountFrom
+			staff.accountRUB = resp.NewAccountTo
+			staff.LastOpPrice = staff.CurrentPrice
+			staff.IsNextOperationBuy = true
+		}
+	}
+	return nil
 }
