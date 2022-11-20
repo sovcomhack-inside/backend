44ecf207f770a25ab9020d0fc75649ca94c97130
senago <mirzoyan.oganes@mail.ru>
2022-11-20 15:14:40 +0300

    [main] add oauth middleware

--- internal/api/api.go
+++ internal/api/api.go
@@ -56,7 +56,7 @@ func NewAPIService(store store.Store) (*APIService, error) {
 	account.POST("/sell", controller.MakeSale)
 
 	oauth := api.Group("/oauth")
-	oauth.GET("/telegram", controller.OAuthTelegram)
+	oauth.GET("/telegram", controller.OAuthTelegram, svc.OAuthTelegramMiddleware)
 
 	admin := api.Group("/admin")
 	admin.POST("/login", controller.LoginAdmin)
