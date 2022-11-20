187afafd40033cb811dfe0e5e25147f3f54f76d6
senago <mirzoyan.oganes@mail.ru>
2022-11-20 17:50:31 +0300

    [main] fix oauth

--- internal/pkg/model/dto/oauth.go
+++ internal/pkg/model/dto/oauth.go
@@ -1,6 +1,6 @@
 package dto
 
 type OAuthTelegramRequest struct {
-	UserID     int64 `query:"param" validate:"required"`
+	UserID     int64 `param:"user_id" validate:"required"`
 	TelegramID int64 `query:"id" validate:"required"`
 }
