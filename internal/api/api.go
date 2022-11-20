6a62a993a1428905f42ba6daa4604459f3a8cb62
ilianovikov <ilianovikov@ozon.ru>
2022-11-20 03:41:51 +0300

    currency data

--- internal/api/api.go
+++ internal/api/api.go
@@ -61,6 +61,7 @@ func NewAPIService(store store.Store) (*APIService, error) {
 
 	currencies := api.Group("/currencies", svc.AuthMiddleware)
 	currencies.GET("/list", controller.ListCurrencies)
+	currencies.GET("/data", controller.GetCurrencyData)
 
 	return svc, nil
 }
